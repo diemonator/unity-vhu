e2c972727529d91b88a7c6e0ff6de23d88ba9f25
Emil <emilkaramihov@gmail.com>
2021-01-05 14:46:58 +0100

    Project

---
+++ UnityRuntimeTextToSpeech-master/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityLibrary;
+
+// A class to help in creating the Json object to be sent to the rasa server
+public class PostMessageJson
+{
+    public string message;
+    public string sender;
+}
+
+[Serializable]
+// A class to extract multiple json objects nested inside a value
+public class RootReceiveMessageJson
+{
+    public ReceiveMessageJson[] messages;
+}
+
+[Serializable]
+// A class to extract a single message returned from the bot
+public class ReceiveMessageJson
+{
+    public string recipient_id;
+    public string text;
+    public string image;
+    public string attachemnt;
+    public string button;
+    public string element;
+    public string quick_replie;
+}
+
+public class NetworkManager : MonoBehaviour
+{
+    private const string rasa_url = "http://localhost:5005/webhooks/rest/webhook";
+
+    public void SendMessageToRasa(string msg)
+    {
+        // Create a json object from user message
+        PostMessageJson postMessage = new PostMessageJson
+        {
+            sender = "user",
+            message = msg
+        };
+
+        string jsonBody = JsonUtility.ToJson(postMessage);
+        print("User json : " + jsonBody);
+
+        // Create a post request with the data to send to Rasa server
+        StartCoroutine(PostRequest(rasa_url, jsonBody));
+    }
+
+    private IEnumerator PostRequest(string url, string jsonBody)
+    {
+        UnityWebRequest request = new UnityWebRequest(url, "POST");
+        byte[] rawBody = new System.Text.UTF8Encoding().GetBytes(jsonBody);
+        request.uploadHandler = new UploadHandlerRaw(rawBody);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        RecieveResponse(request.downloadHandler.text);
+    }
+
+    // Parse the response received from the bot
+    public void RecieveResponse(string response)
+    {
+        Debug.Log(response);
+        // Deserialize response recieved from the bot
+        var recieveMessages =
+            JsonUtility.FromJson<RootReceiveMessageJson>("{\"messages\":" + response + "}");
+
+        // show message based on message type on UI
+        foreach (ReceiveMessageJson message in recieveMessages.messages)
+        {
+            FieldInfo[] fields = typeof(ReceiveMessageJson).GetFields();
+            foreach (FieldInfo field in fields)
+            {
+                string data = null;
+
+                // extract data from response in try-catch for handling null exceptions
+                try
+                {
+                    data = field.GetValue(message).ToString();
+                }
+                catch (NullReferenceException) 
+                {
+                    
+                }
+
+                // print data
+                if (data != null && field.Name != "recipient_id")
+                {
+                    Debug.Log("Bot said \"" + data + "\"");
+                    Speech.instance.Say(data, TTSCallback);
+                }
+            }
+        }
+    }
+
+    void TTSCallback(string message, AudioClip audio)
+    {
+        AudioSource source = GetComponent<AudioSource>();
+        if (source == null)
+        {
+            source = gameObject.AddComponent<AudioSource>();
+        }
+
+        source.clip = audio;
+        source.Play();
+    }
+}
